d7a5931d27002b73c596d02ac31a7f334550447a
paul-aqualicense <paul@aqualicense.com>
2024-01-12 12:21:02 +0000

    mini overhaul

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,7 @@
 module.exports = (eleventyConfig) => {
     eleventyConfig.addPassthroughCopy('src/css');
-    eleventyConfig.addPassthroughCopy('src/js');
     eleventyConfig.addPassthroughCopy('src/assets');
     eleventyConfig.addWatchTarget('src/css');
-    eleventyConfig.addWatchTarget('src/js');
 
     eleventyConfig.addShortcode('year', () => {
         return `${new Date().getFullYear()}`;
